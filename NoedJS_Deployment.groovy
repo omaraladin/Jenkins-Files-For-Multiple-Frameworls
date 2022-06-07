6ab28fd380bd2825d9d18b5fd6a36d16369dd16c
Omar Aladdin <48786827+omaraladin@users.noreply.github.com>
2022-06-07 17:04:19 +0400

    Update NoedJS_Deployment.groovy

--- NoedJS_Deployment.groovy
+++ NoedJS_Deployment.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     environment {
-        DEPLOYMENT_DOMAIN    = 'https://poc.examplevpn.app'
+        DEPLOYMENT_DOMAIN    = 'https://poc.example.app'
         PROJECT              = 'example-web-poc'
         PROJECT_NEW_VERSION  = 'example-web-poc-new-version'
         PROJECT_PREV_VERSION = 'example-web-poc-prev-version'
@@ -10,12 +10,9 @@ pipeline {
         stage('notifyDevelopers') {
             // This simply sends notifications to developers in Telegram
             steps{
-                withCredentials([string(credentialsId: 'chatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {
+                withCredentials([string(credentialsId: 'testChatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {
                 sh ('''
-                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project</b> : ${JOB_NAME} \
-                    <b>Build NO:</b> : ${BUILD_NUMBER} \
-                    <b>Build Status</b>: Pending &#8987; \
-                    The result will be delivered to you when Jenkins Job is Done :D"
+                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project</b> : ${JOB_NAME}\n<b>Build NO:</b> ${BUILD_NUMBER}\n<b>Build Status</b>: Pending ⌛\nThe result will be delivered to you when Jenkins Job is Done :D"
                 ''')
                 }
             }
@@ -23,7 +20,7 @@ pipeline {
         stage('Build') {
             //Thi builds the NodeJS API
             steps {
-                git branch: 'main', credentialsId: '323bb75a-97c0-4f32-809d-c8c4748b584f', url: 'git@192.168.0.201:/root/example-api'
+                git branch: 'main', credentialsId: 'xxxx-xxxx-xxxx-xxxx', url: 'git@gitlab.example.com:/omaraladin/example-api'
                 sh 'npm install'
                 sh 'rm -rf ${PROJECT_NEW_VERSION}.tar.gz'
                 sh 'tar czf ${PROJECT_NEW_VERSION}.tar.gz *'
@@ -32,38 +29,23 @@ pipeline {
     }
     post {
         success {
-            withCredentials([string(credentialsId: 'chatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {
+            withCredentials([string(credentialsId: 'testChatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {
                 sh ('''
-                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project</b> : ${JOB_NAME} \
-                    <b>Build NO:</b> : ${BUILD_NUMBER} \
-                    <b>Build Status</b>: SUCCESS &#9989; \
-                    Check your changes at with an Incognito tab at: \
-                    ${DEPLOYMENT_DOMAIN}"
+                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project</b> : ${JOB_NAME}\n<b>Build NO:</b> ${BUILD_NUMBER}\n<b>Build Status:</b>: SUCCESS ✅ \n<b>Check your changes at with an Incognito tab at: </b>\n${DEPLOYMENT_DOMAIN}"
                 ''')
             }
-            echo 'This has worked well!'
-            echo "${DEPLOYMENT_DOMAIN}"
-            echo "${PROJECT}"
-            echo "${PROJECT_NEW_VERSION}"
-            echo "${PROJECT_PREV_VERSION}"
         }
         failure {
-            withCredentials([string(credentialsId: 'chatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {            
+            withCredentials([string(credentialsId: 'testChatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {            
                 sh ('''
-                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project</b> : ${JOB_NAME} \
-                    <b>Build NO:</b> : ${BUILD_NUMBER} \
-                    <b>Build Status</b>: FAILED &#10060;"
-                ''')            
+                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project</b> : ${JOB_NAME}\n<b>Build NO:</b> ${BUILD_NUMBER} \n<b>Build Status:</b> FAILED ❌"
+                    ''')            
             }
         }
         unstable {
-            withCredentials([string(credentialsId: 'chatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {            
+            withCredentials([string(credentialsId: 'testChatId', variable: 'CHATID'), string(credentialsId: 'botToken', variable: 'TOKEN')]) {            
                 sh ('''
-                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project</b> : ${JOB_NAME} \
-                    <b>Build NO:</b> : ${BUILD_NUMBER} \
-                    <b>Build Status</b>: UNSTABLE &#10071;
-                    Review your changes at: \
-                    ${DEPLOYMENT_DOMAIN}"
+                    curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHATID} -d parse_mode="HTML" -d text="<b>Project:</b> ${JOB_NAME}\n<b>Build NO:</b> ${BUILD_NUMBER}\n<b>Build Status:</b> UNSTABLE ❗ \nReview your changes at:\n${DEPLOYMENT_DOMAIN}"
                 ''')
             }
         }
